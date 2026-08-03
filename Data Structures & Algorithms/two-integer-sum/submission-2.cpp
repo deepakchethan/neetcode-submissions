3cebb7bbd1e8969794e81c3e2eb446823a4fe9f4
Deepak Chethan <deepakchethan@outlook.com>
2026-08-02 18:32:32 -0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        set<int> seen;
+        vector<int> sol;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < nums.size(); ++j) {
+                if (i != j && (nums[i] + nums[j] == target)) {
+                    return {i, j};
+                }
+            }
+        }
+        return sol;
+    }
+};
