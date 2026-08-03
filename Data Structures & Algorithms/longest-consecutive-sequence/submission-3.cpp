2ed0271c9b3d338a6c9fa2967f983eeefc8735f5
Deepak Chethan <deepakchethan@outlook.com>
2026-08-03 07:08:44 -0700

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int maxCount = 1;
+        int count = 1;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == nums[i+1]) {
+                continue;
+            }
+            if (nums[i] + 1 == nums[i+1]) {
+                count++;
+                if (maxCount < count) {
+                    maxCount = count;
+                }
+            } else {
+                count = 1;
+            }
+        }
+        return maxCount;
+    }
+};
